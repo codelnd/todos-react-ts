78cea5a9998b44a968a1b0406c1d6e56f6729be6
Denis Bezukladnikov <denis3094@mail.ru>
2022-10-16 03:36:57 +0700

    feat: реализовать показ задач в списке, стилизация списка

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -14,17 +14,32 @@ const App = () => {
             isComplete: false
         }
         setTasks([...tasks, task])
+
     }
 
     return (
         <div className="container">
             <form id='todoForm' name='todoForm' className='input-field' onSubmit={submitHandler}>
                 <input ref={inputRef} type='text' id='todo' name='todo'
-                       placeholder='Введите необходимую задачу'/>
+                       placeholder='Введите необходимую задачу' autoComplete='off'/>
                 <label htmlFor='todo' className='active'>Добавить задачу</label>
             </form>
             <div className='todo-list'>
                 <p className='todo-list__empty'>Список задач пуст</p>
+                <ul className='todo-list__tasks'>
+                    {tasks.map(el => {
+                        return (
+                            <li className='todo-list__task' key={el.id}>
+                                <label className='todo-list__task-wrp'>
+                                    <input type="checkbox"
+                                           checked={el.isComplete}/>
+                                    <span className='todo-list__complete todo-list__complete_decorated'>{el.title}</span>
+                                    <i className='material-icons black-text'>clear</i>
+                                </label>
+                            </li>
+                        )
+                    })}
+                </ul>
             </div>
         </div>
     );
