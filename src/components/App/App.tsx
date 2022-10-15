069f5ff5913ff230298de6e890e9c870f1542171
Denis Bezukladnikov <denis3094@mail.ru>
2022-10-16 02:26:55 +0700

    feat: слушать инпут через useref, написать функцию добавления задачи

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,25 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {Form} from "../Form/Form";
 import {ITodoEl} from "../../utils/models";
 
 const App = () => {
     const [tasks, setTasks] = useState<ITodoEl[]>([]);
+    const inputRef = useRef<HTMLInputElement>(null)
 
-
+    const submitHandler = (e: React.ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const task = {
+            id: Date.now(),
+            title: inputRef.current!.value,
+            isComplete: false
+        }
+        setTasks([...tasks, task])
+    }
+  
     return (
         <div className="container">
-            <form id='todoForm' name='todoForm' className='input-field'>
-                <input type='text' id='todo' name='todo'
+            <form id='todoForm' name='todoForm' className='input-field' onSubmit={submitHandler}>
+                <input ref={inputRef} type='text' id='todo' name='todo'
                        placeholder='Введите необходимую задачу'/>
                 <label htmlFor='todo' className='active'>Добавить задачу</label>
             </form>
