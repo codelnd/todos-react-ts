3aea15e5777d74437abbf6ec86c588fe8c68cc85
Denis Bezukladnikov <denis3094@mail.ru>
2022-10-16 21:18:57 +0700

    fix: удалить неиспользуемый импорт в app.js

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Form } from "../Form/Form";
 import { ITodoEl } from "../../utils/models";
 import { TodoList } from "../TodoList/TodoList";
