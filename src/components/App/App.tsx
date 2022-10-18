738abc50313e8efa46ad559f7a0cfb2401a0b150
Denis Bezukladnikov <denis3094@mail.ru>
2022-10-18 15:53:13 +0700

    feat: импортировать хук в app.js

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,20 +2,25 @@ import React, { useState, useEffect } from "react";
 import { Form } from "../Form/Form";
 import { ITodoEl } from "../../utils/models";
 import { TodoList } from "../TodoList/TodoList";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
 
 const App = () => {
   const [tasks, setTasks] = useState<ITodoEl[]>([]);
+  const [storageTasks, setStorageTasks] = useLocalStorage({
+    initialValue: tasks,
+    key: "tasks",
+  });
 
-  useEffect(() => {
-    const tasksFromStorage = JSON.parse(
-      localStorage.getItem("tasks") || "[]"
-    ) as ITodoEl[];
-    setTasks(tasksFromStorage);
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }, [tasks]);
+  // useEffect(() => {
+  //   const tasksFromStorage = JSON.parse(
+  //     localStorage.getItem("tasks") || "[]"
+  //   ) as ITodoEl[];
+  //   setTasks(tasksFromStorage);
+  // }, []);
+  //
+  // useEffect(() => {
+  //   localStorage.setItem("tasks", JSON.stringify(tasks));
+  // }, [tasks]);
 
   return (
     <div className="container">
