b4cbf7a379a11a5d78a567d603174d2d769f2082
Denis Bezukladnikov <denis3094@mail.ru>
2022-10-19 21:20:45 +0700

    feat: вынести список в отдельный компонент todolistelements

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ITodo } from "../../utils/models";
 import { EmptyList } from "../EmptyList/EmptyList";
+import { TodoListElements } from "../TodoListElements/TodoListElements";
 
 interface IFormProps {
   tasks: ITodo[];
@@ -29,33 +30,11 @@ export const TodoList = ({ tasks, onTasks }: IFormProps) => {
       {tasks.length === 0 ? (
         <EmptyList />
       ) : (
-        <ul className="todo-list__tasks">
-          {tasks.map((el) => {
-            const taskClass = ["todo-list__task"];
-            if (el.isComplete) {
-              taskClass.push("todo-list__task_complete");
-            }
-
-            return (
-              <li className={taskClass.join(" ")} key={el.id}>
-                <label className="todo-list__task-wrp">
-                  <input
-                    type="checkbox"
-                    checked={el.isComplete}
-                    onChange={completeHandler.bind(null, el.id)}
-                  />
-                  <span className="todo-list__title">{el.title}</span>
-                  <i
-                    className="material-icons black-text"
-                    onClick={removeHandler.bind(null, el.id)}
-                  >
-                    clear
-                  </i>
-                </label>
-              </li>
-            );
-          })}
-        </ul>
+        <TodoListElements
+          tasks={tasks}
+          onRemove={removeHandler}
+          onComplete={completeHandler}
+        />
       )}
     </div>
   );
