b68e8a2ee9e1afa71e56fb218cf6e75b5a1610ba
Denis Bezukladnikov <denis3094@mail.ru>
2022-11-07 21:37:47 +0700

    feat: добавить стили к компоненту container

--- src/components/Container/Container.style.ts
+++ src/components/Container/Container.style.ts
@@ -1,3 +1,9 @@
 import styled from "styled-components";
 
-export const ContainerStyle = styled.div``;
+export const ContainerStyle = styled.div`
+  outline: 1px solid #e8e8e8;
+  border-radius: 10px;
+  padding: 1px 20px;
+  margin-top: 50px;
+  min-height: 80vh;
+`;
