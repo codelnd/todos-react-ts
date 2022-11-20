b0e3193b0b799cc36e56e221e036ec5af002b095
Denis Bezukladnikov <denis3094@mail.ru>
2022-11-20 23:15:04 +0700

    feat: обернуть приложение в provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 reportWebVitals();
