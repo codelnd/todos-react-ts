7fe3a5ae8b4d2159efd2092b96cdb69f9ff952f2
Denis Bezukladnikov <denis3094@mail.ru>
2022-11-20 23:06:07 +0700

    feat: создать rootreducer, передать в store, export default store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,3 +1,6 @@
 import { createStore } from "redux";
+import { rootReducer } from "../reducers/rootReducer";
 
-const store = createStore();
+const store = createStore(rootReducer);
+
+export default store;
