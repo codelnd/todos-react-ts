689d1b4786d1c5efffe52a1d37e34bb35ecbc423
Denis Bezukladnikov <denis3094@mail.ru>
2022-11-20 23:24:03 +0700

    feat: вывести state в компонент app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,12 +3,14 @@ import { TodoList } from "../TodoList/TodoList";
 import { useLocalStorage } from "../../hooks/useLocalStorage";
 import { TodoTitle } from "../TodoTitle/TodoTitle";
 import { Container } from "../Container/Container";
+import { useSelector } from "react-redux";
 
 const App = () => {
   const [tasks, setTasks] = useLocalStorage({
     initialValue: [],
     key: "tasks",
   });
+  const state = useSelector((state) => state);
 
   return (
     <Container>
