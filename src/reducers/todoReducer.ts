ad8056eb7e9ce84db4036f0d8f2d645747bb2ebc
Denis Bezukladnikov <denis3094@mail.ru>
2022-11-20 22:11:17 +0700

    feat: создать начальный стейт, типизировать его

--- src/reducers/todoReducer.ts
+++ src/reducers/todoReducer.ts
@@ -1,5 +1,20 @@
-export const todoReducer = (state, action) => {
+const initialState: [] | Array<object> = [];
+
+export const todoReducer = (state = initialState, action) => {
   switch (action.type) {
+    case "ADD_TODO":
+      return [
+        ...state,
+        {
+          id: Date.now(),
+          title: action.payload,
+          isComplete: false,
+        },
+      ];
+    case "COMPLETE_TODO":
+      return;
+    case "REMOVE_TODO":
+      return;
     default:
       return state;
   }
