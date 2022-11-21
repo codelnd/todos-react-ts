db03de6f81813313f366499fa197f2e3f4e36eb7
Denis Bezukladnikov <denis3094@mail.ru>
2022-11-21 16:24:13 +0700

    feat: удалить лишний код из компонента app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,11 @@
 import { Form } from "../Form/Form";
 import { TodoList } from "../TodoList/TodoList";
-import { useLocalStorage } from "../../hooks/useLocalStorage";
 import { TodoTitle } from "../TodoTitle/TodoTitle";
 import { Container } from "../Container/Container";
-import { useSelector } from "react-redux";
 import { useState } from "react";
 
 const App = () => {
   const [tasks, setTasks] = useState<any[]>([]);
-
-  const state = useSelector((state) => state);
 
   return (
     <Container>
