8ac85f97811f50c95245aa5183b24d5e345cff5f
Denis Bezukladnikov <denis3094@mail.ru>
2022-11-21 16:38:42 +0700

    refactor: вывести количество дел из стейта redux

--- src/components/TodoTitle/TodoTitle.tsx
+++ src/components/TodoTitle/TodoTitle.tsx
@@ -4,5 +4,5 @@ import useTypedSelector from "../../hooks/useTypedSelector";
 export const TodoTitle = () => {
   const state = useTypedSelector((state) => state.todo);
 
-  return <TodoTitleStyle>{`Количество дел: 0`}</TodoTitleStyle>;
+  return <TodoTitleStyle>{`Количество дел: ${state.length}`}</TodoTitleStyle>;
 };
