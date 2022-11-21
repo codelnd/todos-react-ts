8e6e3e174f2a32ec144e8535c019649bf979d87d
Denis Bezukladnikov <denis3094@mail.ru>
2022-11-21 17:20:03 +0700

    refactor: вывести данные из стейта redux в компоненте todolistelements

--- src/components/TodoListElements/TodoListElements.tsx
+++ src/components/TodoListElements/TodoListElements.tsx
@@ -1,21 +1,15 @@
-import React from "react";
-import { ITodo } from "../../models/models";
 import { TodoListElLabel, TodoListElTitle } from "./TodoListElements.style";
+import useTypedSelector from "../../hooks/useTypedSelector";
+import { useDispatch } from "react-redux";
+import { completeTodo, removeTodo } from "../../action-creators/actionCreators";
 
-interface ITodoListElementsProps {
-  tasks: ITodo[];
-  onRemove: (id: number) => void;
-  onComplete: (id: number) => void;
-}
+export const TodoListElements = () => {
+  const state = useTypedSelector((state) => state.todo);
+  const dispatch = useDispatch();
 
-export const TodoListElements = ({
-  tasks,
-  onRemove,
-  onComplete,
-}: ITodoListElementsProps) => {
   return (
     <ul className="todo-list__tasks">
-      {tasks.map((el) => {
+      {state.map((el) => {
         const taskClass = ["todo-list__task"];
         if (el.isComplete) {
           taskClass.push("todo-list__task_complete");
@@ -27,7 +21,7 @@ export const TodoListElements = ({
               <input
                 type="checkbox"
                 checked={el.isComplete}
-                onChange={onComplete.bind(null, el.id)}
+                onChange={() => dispatch(completeTodo(el.id))}
               />
               <TodoListElTitle
                 primary={el.isComplete}
@@ -38,7 +32,7 @@ export const TodoListElements = ({
               </TodoListElTitle>
               <i
                 className="material-icons black-text"
-                onClick={onRemove.bind(null, el.id)}
+                onClick={() => dispatch(removeTodo(el.id))}
               >
                 clear
               </i>
