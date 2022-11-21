585ca272384c36c03d39853573e7116f689b7a5c
Denis Bezukladnikov <denis3094@mail.ru>
2022-11-21 17:11:11 +0700

    refactor: вывести данные из стейта redux в компоненте todolist

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,16 +2,13 @@ import { Form } from "../Form/Form";
 import { TodoList } from "../TodoList/TodoList";
 import { TodoTitle } from "../TodoTitle/TodoTitle";
 import { Container } from "../Container/Container";
-import { useState } from "react";
 
 const App = () => {
-  const [tasks, setTasks] = useState<any[]>([]);
-
   return (
     <Container>
       <TodoTitle />
       <Form />
-      <TodoList tasks={tasks} onTasks={setTasks} />
+      <TodoList />
     </Container>
   );
 };
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,41 +1,13 @@
-import React from "react";
-import { ITodo } from "../../models/models";
 import { EmptyList } from "../EmptyList/EmptyList";
 import { TodoListElements } from "../TodoListElements/TodoListElements";
+import useTypedSelector from "../../hooks/useTypedSelector";
 
-interface IFormProps {
-  tasks: ITodo[];
-
-  onTasks(p: (prevState: ITodo[]) => ITodo[]): void;
-}
-
-export const TodoList = ({ tasks, onTasks }: IFormProps) => {
-  const removeHandler = (id: number): void => {
-    onTasks((prevState: ITodo[]) => prevState.filter((el) => el.id !== id));
-  };
-
-  const completeHandler = (id: number): void => {
-    onTasks((prevState) =>
-      prevState.map((el) => {
-        if (el.id === id) {
-          el.isComplete = !el.isComplete;
-        }
-        return el;
-      })
-    );
-  };
+export const TodoList = () => {
+  const state = useTypedSelector((state) => state.todo);
 
   return (
     <div className="todo-list">
-      {tasks.length === 0 ? (
-        <EmptyList />
-      ) : (
-        <TodoListElements
-          tasks={tasks}
-          onRemove={removeHandler}
-          onComplete={completeHandler}
-        />
-      )}
+      {state.length === 0 ? <EmptyList /> : <TodoListElements />}
     </div>
   );
 };
