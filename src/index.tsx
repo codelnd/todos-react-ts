c55f55a635a5f63e96fda86eaf5f383e4e736e31
Denis Bezukladnikov <denis3094@mail.ru>
2022-11-21 17:58:07 +0700

    feat: создать persist-store

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,17 @@ import App from "./components/App/App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./store/persistStore";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
 reportWebVitals();
